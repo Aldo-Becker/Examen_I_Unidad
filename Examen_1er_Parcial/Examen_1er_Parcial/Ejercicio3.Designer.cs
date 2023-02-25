5378a7b2e76b07fd3c89cea973225a079861b903
Aldo-Becker <113803505+Aldo-Becker@users.noreply.github.com>
2023-02-24 22:38:27 -0600

    Creación Ejercicio 3

--- Examen_1er_Parcial/Examen_1er_Parcial/Ejercicio3.Designer.cs
+++ Examen_1er_Parcial/Examen_1er_Parcial/Ejercicio3.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.PresionarButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // PresionarButton
+            // 
+            this.PresionarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PresionarButton.Location = new System.Drawing.Point(413, 244);
+            this.PresionarButton.Name = "PresionarButton";
+            this.PresionarButton.Size = new System.Drawing.Size(120, 49);
+            this.PresionarButton.TabIndex = 0;
+            this.PresionarButton.Text = "Presionar";
+            this.PresionarButton.UseVisualStyleBackColor = true;
+            this.PresionarButton.Click += new System.EventHandler(this.PresionarButton_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(43, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(236, 537);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Ejercicio3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 552);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.PresionarButton);
+            this.Name = "Ejercicio3";
             this.Text = "Ejercicio3";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button PresionarButton;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
--- Examen_1er_Parcial/Examen_1er_Parcial/Ejercicio3.cs
+++ Examen_1er_Parcial/Examen_1er_Parcial/Ejercicio3.cs
@@ -16,5 +16,32 @@ namespace Examen_1er_Parcial
         {
             InitializeComponent();
         }
+
+        private void PresionarButton_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(RecorrerNumeros(1));
+        }
+        private int RecorrerNumeros(int numeroInicio)
+        {
+
+            while (numeroInicio < 100)
+            {
+                listBox1.Items.Add(numeroInicio);
+                if (numeroInicio % 3 == 0)
+                {
+                    listBox1.Items.Add("Aldo");
+                }
+                if (numeroInicio % 5 == 0)
+                {
+                    listBox1.Items.Add("Becker");
+                }
+                if (numeroInicio % 3 == 0 && numeroInicio % 5 == 0)
+                {
+                    listBox1.Items.Add("Aldo Becker");
+                }
+                numeroInicio++;
+            }
+            return numeroInicio;
+        }
     }
 }
